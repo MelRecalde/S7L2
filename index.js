ddc2413c26830e312851446edae54c2216afeeff
MelRecalde <melquisedicrecalde@gmail.com>
2024-06-11 17:17:23 +0200

    save card

--- index.js
+++ index.js
@@ -1,27 +1,46 @@
 const form = document.querySelector("form");
 
+const card = document.querySelector(".card")
+
 const inputName = document.getElementById("name");
 const saveBtn = document.getElementById("save");
 const deleteBtn = document.getElementById("delete");
 
 let names = [];
 
+/*
 class Name {
     constructor(name) {
         this.userName = name;
     }
 }
+*/
 
-saveBtn.onsubmit = event => {
+const generateCard = () => {
+    const p = document.createElement("p")
+    const hr = document.createElement("hr")
+    p.innerText = inputName.value
+
+    card.appendChild(p)
+    card.append(hr)
+}
+
+
+
+form.onsubmit = (event) => {
     event.preventDefault();
 
-const newName = new Name(inputName.value)
+const newName = inputName.value
+console.log(newName)
 
 names.push(newName)
+// console.log(names)
+localStorage.setItem("userName", JSON.stringify(names))
 
-localStorage.setItem("userName", JSON.stringify(newName))
-
+generateCard()
 
 }
+
+
 
 
