6e04b391103f17ac814723daa0e384bab356d60e
MelRecalde <melquisedicrecalde@gmail.com>
2024-06-12 13:49:25 +0200

    add event listeners

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const form = document.querySelector("form");
 
-const card = document.querySelector(".card")
+const list = document.querySelector(".list")
 
 const inputName = document.getElementById("name");
 const saveBtn = document.getElementById("save");
@@ -17,30 +17,58 @@ class Name {
 */
 
 const generateCard = () => {
-    const p = document.createElement("p")
-    const hr = document.createElement("hr")
-    p.innerText = inputName.value
-
-    card.appendChild(p)
-    card.append(hr)
+    const li = document.createElement("li")
+    // const hr = document.createElement("hr")
+    li.innerText = inputName.value
+    list.appendChild(li)
+    // list.append(hr)
 }
-
-
 
 form.onsubmit = (event) => {
     event.preventDefault();
 
-const newName = inputName.value
-console.log(newName)
+    const newName = inputName.value
+    names.push(newName)
+    // console.log(names)
+    localStorage.setItem("userName", JSON.stringify(names))
 
-names.push(newName)
-// console.log(names)
-localStorage.setItem("userName", JSON.stringify(names))
+  /*  if (saveBtn) {
+        localStorage.getItem("userName")
+        generateCard()
+        list.innerText = inputName.value
+    } else if (deleteBtn) {
+        localStorage.removeItem("userName")
+        list.innerText = ""
+    } else {
+        list.innerText = "No names in data"
+    } */
 
-generateCard()
+    saveBtn.addEventListener("click", () => {
+        localStorage.getItem("userName")
+        generateCard()
+    })
+
+    deleteBtn.addEventListener("click", () => {
+        localStorage.removeItem("userName")
+        names.pop()
+    })
+
 
 }
 
+// EX 2
 
+let i = sessionStorage.getItem('count')
+        ? parseInt(sessionStorage.getItem('count'))
+        : 0
+
+      const counter = function () {
+        i = i + 1
+        document.getElementById('timer').innerText =
+          i + ' seconds'
+        sessionStorage.setItem('count', i)
+      }
+
+      setInterval(counter, 1500)
 
 
